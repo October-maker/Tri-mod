53b66926a3f50f873b66992974800e49fa93beee
october <Day19ZZZ@126.com>
2026-06-27 09:26:43 +0800

    a

--- src/triangle/black/projection/TriPJBlock.java
+++ src/triangle/black/projection/TriPJBlock.java
@@ -2,66 +2,16 @@ package triangle.black.projection;
 
 import mindustry.gen.Building;
 import mindustry.world.Block;
-import triangle.black.projection.num.PJBlockNumber;
 
 public class TriPJBlock extends Block {
-
-    static {
-        PJBlockNumber.get();
-    }
 
     public TriPJBlock(String name){
         super(name);
         buildCostMultiplier = 0;
     }
 
+    int bitCost = 2;
+
     public class TriPJBlockBuilding extends Building {}
 
-//    public int FacMaxbuild() {
-//        if (PJCoreNumber.get() < 3) {
-//            return PJCoreNumber.get() * 5;
-//        }else{
-//            return 20;
-//        }
-//    }
-
-//    @Override
-//    public boolean canPlaceOn(Tile tile, Team team, int rotation) {
-//        return PJBlockNumber.get() < FacMaxbuild();
-//    }
-
-//    @Override
-//    public void setBars() {
-//        super.setBars();
-//        addBar("PJFactory",(TriPJBlock.TriPJFactoryBuild e) -> new Bar(
-//                () -> Core.bundle.get("inBulidPJFactory") + PJBlockNumber.get() + "/" + FacMaxbuild(),
-//                () -> Color.valueOf("FFFFFF"),
-//                () -> (float) PJBlockNumber.get() / FacMaxbuild()
-//        ));
-//    }
-
-//    public class TriPJFactoryBuild extends Building{
-//
-//        @Override
-//        public void created() {
-//            super.created();
-//            PJBlockNumber.refresh();
-//        }
-//
-//        @Override
-//        public void afterDestroyed() {
-//            super.afterDestroyed();
-//            PJBlockNumber.refresh();
-//        }
-//
-//        int timer = 0;
-//        @Override
-//        public void updateTile() {
-//            timer++;
-//            if (timer >= 60) {
-//                PJBlockNumber.refresh();
-//                timer = 0;
-//            }
-//        }
-//    }
 }
